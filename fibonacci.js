0a916d414c2cc3a0b20f3ee88e3cbd150f97e23d
rutanga <phiphirutanga@gmail.com>
2025-07-09 18:07:26 +0300

    Added a refactor comment for refactoring our code, just incase we need to

--- fibonacci.js
+++ fibonacci.js
@@ -9,4 +9,14 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+
+// Iterative approach (when we need to refactor our code)
+// let a = 0, b = 1;
+// for (let i = 2; i <= n; i++) {
+//     let temp = a + b;
+//     a = b;
+//     b = temp;
+// }
+// return b;
+
 module.exports = fibonacci;
